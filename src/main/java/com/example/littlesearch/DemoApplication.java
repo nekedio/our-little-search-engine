ea0d95df378f0a85bc3955ad7adf71b92ed0353b
fyezhlo <fyezhlo@gmail.com>
2023-02-05 15:47:27 +0300

    log into file (file generates automatically)

--- src/main/java/com/example/littlesearch/DemoApplication.java
+++ src/main/java/com/example/littlesearch/DemoApplication.java
@@ -2,15 +2,12 @@ package com.example.littlesearch;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import static org.apache.logging.log4j.Logger.*;
-
 @SpringBootApplication
 public class DemoApplication {
-    private static Logger logger = LogManager.getLogger(DemoApplication.class.getName());
+    private static final Logger logger = LogManager.getLogger(DemoApplication.class.getName());
 
 
     public static void main(String[] args) {
