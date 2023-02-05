49771b3530c9689cf6af81fae2000f4ed5c8cb8f
fyezhlo <fyezhlo@gmail.com>
2023-02-05 12:58:35 +0300

    console logging

--- src/main/java/com/example/littlesearch/DemoApplication.java
+++ src/main/java/com/example/littlesearch/DemoApplication.java
@@ -1,13 +1,22 @@
 package com.example.littlesearch;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.apache.logging.log4j.Logger.*;
+
 @SpringBootApplication
 public class DemoApplication {
+    private static Logger logger = LogManager.getLogger(DemoApplication.class.getName());
+
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
+        logger.info("testing logging");
     }
 
 }
