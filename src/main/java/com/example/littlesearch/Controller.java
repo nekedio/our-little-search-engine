afe997657cf3560a31e425056c4b5efbce2e7080
Nikita Ionov <ionov.goo@gmail.com>
2023-02-10 08:45:57 +0300

    fixed errors

--- src/main/java/com/example/littlesearch/Controller.java
+++ src/main/java/com/example/littlesearch/Controller.java
@@ -1,12 +1,21 @@
 package com.example.littlesearch;
 
+import com.example.littlesearch.config.SitesList;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    @Autowired
+    private SitesList sitesList;
+
     @GetMapping("/hello")
     public String getHello() {
+        System.out.println("*************************************");
+        System.out.println(sitesList.getSitesList());
+        System.out.println("*************************************");
+
         return "Hello!";
     }
 }
--- src/main/java/com/example/littlesearch/config/SitesList.java
+++ src/main/java/com/example/littlesearch/config/SitesList.java
@@ -3,13 +3,13 @@ package com.example.littlesearch.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
 @Setter
 @Getter
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "indexing-settings")
 public class SitesList {
     private List<Site> sitesList;
